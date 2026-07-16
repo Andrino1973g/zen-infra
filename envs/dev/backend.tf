598833da845c020e3b178b2108e046f4400a2a19
Nnamdi <nokeke67@gmail.com>
2026-07-16 08:34:34 -0500

    initial commit

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-andrino1973g"
+    bucket       = "zen-pharma-tfstate-nnamdi"
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
